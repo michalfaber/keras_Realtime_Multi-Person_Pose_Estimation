d1a114016b641124942db265d30b431c496c3faa
Govind <govindnh4cl@users.noreply.github.com>
2018-05-31 15:42:43 -0400

    Fix in config_reader.py
    Convert map() output to list to preserve the values for subsequent calls. Needed when trying to run on batch mode with multiple images.

--- config_reader.py
+++ config_reader.py
@@ -17,7 +17,7 @@ def config_reader():
     param['use_gpu'] = int(param['use_gpu'])
     param['starting_range'] = float(param['starting_range'])
     param['ending_range'] = float(param['ending_range'])
-    param['scale_search'] = map(float, param['scale_search'])
+    param['scale_search'] = list(map(float, param['scale_search']))
     param['thre1'] = float(param['thre1'])
     param['thre2'] = float(param['thre2'])
     param['thre3'] = float(param['thre3'])
